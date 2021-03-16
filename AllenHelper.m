6c5924209a2e8823ddab7eedfea583e4dc252532
priyanka gupta <speak2priya@gmail.com>
2021-03-16 09:40:49 -0400

    Allen sections from old atlas for slices anterior to bregma

---
+++ AllenHelper.m
@@ -0,0 +1,80 @@
+% Load the URL
+
+
+for i = 1:30
+%     3.045 = 102162422       
+%     2.945 = 102162418    
+%     2.845 = 102162414
+%     2.745 = 102162410
+%     2.620 = 102162407
+%     2.545 = 102162402
+%     2.445 = 102162398
+%     2.345 = 102162394
+%     2.245 = 102162390
+%     2.145 = 102162386
+%     2.045 = 102162382
+%     1.945 = 102162378
+%     1.845 = 102162374
+%     1.745 = 102162370
+%     1.645 = 102162366
+%     1.545 = 102162362
+%     1.42  = 102162359
+%     1.345 = 102162354
+%     1.245 = 102162350
+%     1.145 = 102162346
+%     1.045 = 102162342
+%     0.945 = 102162338
+%     0.845 = 102162334
+%     0.745 = 102162330
+%     0.620 = 102162327
+%     0.545 = 102162322
+%     0.445 = 102162318
+%     0.345 = 102162314
+%     0.245 = 102162310
+%     0.145 = 102162306
+
+%     0.020 = 102162303
+% -2.255 = 102162210
+% -2.355 = 102162206
+% -2.048 = 102162203
+
+    
+ImageID = [102162422, 102162418, 102162414, 102162410, 102162407, ...
+           102162402, 102162398, 102162394, 102162390, 102162386, ...
+           102162382, 102162378, 102162374, 102162370, 102162366, ...
+           102162362, 102162359, 102162354, 102162350, 102162346, ...
+           102162342, 102162338, 102162334, 102162330, 102162327, ...
+           102162322, 102162318, 102162314, 102162310, 102162306, ...
+           ];
+MyURL = ['https://mouse.brain-map.org/experiment/siv?id=100142143&imageId=',...
+        num2str(ImageID(i)),...
+        '&imageType=atlas&initImage=atlas&showSubImage=y&contrast=0.5,0.5,0,255,4'];
+web(MyURL,'-noaddressbox','-notoolbar')
+pause(10);
+
+% Take screen capture
+robot = java.awt.Robot();
+pos = [434 182 553 368]; % [left top width height] 1036, 605
+rect = java.awt.Rectangle(pos(1),pos(2),pos(3),pos(4));
+cap = robot.createScreenCapture(rect);
+
+% Convert to an RGB image
+rgb = typecast(cap.getRGB(0,0,cap.getWidth,cap.getHeight,[],0,cap.getWidth),'uint8');
+imgData = zeros(cap.getHeight,cap.getWidth,3,'uint8');
+imgData(:,:,1) = reshape(rgb(3:4:end),cap.getWidth,[])';
+imgData(:,:,2) = reshape(rgb(2:4:end),cap.getWidth,[])';
+imgData(:,:,3) = reshape(rgb(1:4:end),cap.getWidth,[])';
+%imshow(imgData)
+
+Sections(i,:,:,:) = imgData;
+
+% Show or save to file
+
+%imwrite(imgData,'out.png')
+
+end
+
+%zero = 280;
+% going left use 47, going right use 48
+
+% imshow(squeeze(Sections(10,:,:,:))); hold on; line(280*[1 1],[67 350],'color','k')
---
+++ Allen_PG.m
@@ -0,0 +1,164 @@
+function varargout = Allen_PG(varargin)
+% ALLEN_PG MATLAB code for Allen_PG.fig
+%      ALLEN_PG, by itself, creates a new ALLEN_PG or raises the existing
+%      singleton*.
+%
+%      H = ALLEN_PG returns the handle to a new ALLEN_PG or the handle to
+%      the existing singleton*.
+%
+%      ALLEN_PG('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in ALLEN_PG.M with the given input arguments.
+%
+%      ALLEN_PG('Property','Value',...) creates a new ALLEN_PG or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Allen_PG_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Allen_PG_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Allen_PG
+
+% Last Modified by GUIDE v2.5 15-Mar-2021 22:49:06
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Allen_PG_OpeningFcn, ...
+                   'gui_OutputFcn',  @Allen_PG_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Allen_PG is made visible.
+function Allen_PG_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Allen_PG (see VARARGIN)
+
+% Choose default command line output for Allen_PG
+handles.output = hObject;
+
+handles.Coordinates.Data(1) = varargin{1};
+handles.Coordinates.Data(2) = varargin{2};
+handles.Coordinates.Data(3) = varargin{3};
+
+UpdateSection_Callback(hObject, eventdata, handles);
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Allen_PG wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Allen_PG_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in UpdateSection.
+function UpdateSection_Callback(hObject, eventdata, handles)
+% hObject    handle to UpdateSection (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+%imshow(squeeze(Sections(24,:,:,:))); hold on; line(280*[1 1],[67 350],'color','k');
+% display webcam image, if available
+
+load('/Users/Priyanka/Desktop/LABWORK_II/Data/AllenCCF_PG/AllenSections_AON_APC.mat');
+
+% find the relevant section
+[~,imageID] = min(abs(AP - handles.Coordinates.Data(1)));
+axes(handles.MySection);
+handles.current_section = image(squeeze(Sections(imageID,:,:,:)),'parent',handles.MySection);
+set(handles.MySection,'XTick',[],'XTickLabel',' ','XTickMode','manual','XTickLabelMode','manual');
+set(handles.MySection,'YTick',[],'YTickLabel',' ','YTickMode','manual','YTickLabelMode','manual');
+
+% draw a vertical line along the ML coordinate on either side
+ML_left = Scale.ML.zero + Scale.ML.left*handles.Coordinates.Data(2);
+ML_right = Scale.ML.zero + Scale.ML.right*handles.Coordinates.Data(2);
+handles.ML_L = line(ML_left*[1 1], [1 368], 'color', 'r');
+handles.ML_R = line(ML_right*[1 1], [1 368], 'color', 'r');
+
+% mark the tetrode location based on depth
+% define surface
+if isempty(Surface{imageID})
+    handles.Msg.String = 'Mark Surface';
+    [~,yi] = getpts(handles.MySection);
+    % ignore x, keep y
+    Surface{imageID}(1,:) = [handles.Coordinates.Data(2), yi];
+    save('/Users/Priyanka/Desktop/LABWORK_II/Data/AllenCCF_PG/AllenSections_AON_APC.mat','AP','Scale','Sections','Surface');
+    handles.Msg.String = '';
+else
+    if ~isempty(find(Surface{imageID}(:,1)==handles.Coordinates.Data(2)))
+        yi = Surface{imageID}(find(Surface{imageID}(:,1)==handles.Coordinates.Data(2)),2);
+    else
+        handles.Msg.String = 'Mark Surface';
+        [~,yi] = getpts(handles.MySection);
+        % ignore x, keep y
+        Surface{imageID} = vertcat(Surface{imageID},[handles.Coordinates.Data(2), yi]);
+        save('/Users/Priyanka/Desktop/LABWORK_II/Data/AllenCCF_PG/AllenSections_AON_APC.mat','AP','Scale','Sections','Surface');
+        handles.Msg.String = '';
+    end
+end
+% surface
+handles.surface = line([1 553], [yi yi], 'color', 'b');
+handles.DV = line([1 553], yi + Scale.DV*handles.Coordinates.Data(3)*[1 1], 'color', 'r');
+guidata(hObject, handles);
+
+
+% --- Executes on button press in EditSurface.
+function EditSurface_Callback(hObject, eventdata, handles)
+% hObject    handle to EditSurface (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+load('/Users/Priyanka/Desktop/LABWORK_II/Data/AllenCCF_PG/AllenSections_AON_APC.mat');
+% find the relevant section
+[~,imageID] = min(abs(AP - handles.Coordinates.Data(1)));
+
+handles.Msg.String = 'Mark Surface';
+[~,yi] = getpts(handles.MySection);
+% ignore x, keep y
+foo = find(Surface{imageID}(:,1)==handles.Coordinates.Data(2));
+Surface{imageID}(foo,2) = yi;
+save('/Users/Priyanka/Desktop/LABWORK_II/Data/AllenCCF_PG/AllenSections_AON_APC.mat','AP','Scale','Sections','Surface');
+% update surface
+handles.surface.YData = [yi yi];
+handles.Msg.String = '';
+guidata(hObject, handles);
+
+
+% --- Executes on button press in ShowSurface.
+function ShowSurface_Callback(hObject, eventdata, handles)
+% hObject    handle to ShowSurface (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if get(hObject,'Value')
+    handles.surface.LineStyle = '-';
+else
+    handles.surface.LineStyle = 'none';
+end
+% Hint: get(hObject,'Value') returns toggle state of ShowSurface
+guidata(hObject, handles);
